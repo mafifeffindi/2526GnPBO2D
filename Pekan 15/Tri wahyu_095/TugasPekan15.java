2bab9f491054422d83779208b120011e25317e12
Yuyanikko <triwahyufajarmalik496@gmail.com>
2026-06-04 18:13:04 +0700

    TugasPekan15.java

--- Pekan 15/Tri wahyu_095/TugasPekan15.java
+++ Pekan 15/Tri wahyu_095/TugasPekan15.java
@@ -1,1 +1,38 @@
+class Kendaraan {
+    void bergerak() {
+        System.out.println("Kendaraan sedang bergerak");
+    }
+}
 
+// Subclass Mobil
+class Mobil extends Kendaraan {
+    @Override
+    void bergerak() {
+        System.out.println("Mobil berjalan di jalan raya");
+    }
+}
+
+// Subclass Motor
+class Motor extends Kendaraan {
+    @Override
+    void bergerak() {
+        System.out.println("Motor melaju dengan cepat");
+    }
+}
+
+// Main Class
+public class Main {
+    public static void main(String[] args) {
+
+        // Inheritance
+        Mobil mobil = new Mobil();
+        mobil.bergerak();
+
+        // Polymorphism
+        Kendaraan k1 = new Mobil();
+        Kendaraan k2 = new Motor();
+
+        k1.bergerak();
+        k2.bergerak();
+    }
+}
