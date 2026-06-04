db7a7cc3c38fe6da336d1fb55be1d50ca0c7766f
zhilviaqnmk-code <zhilviaqnmk@icloud.com>
2026-06-04 19:26:33 +0700

    Update main.java

--- Pekan 03/zhilvia_084/main.java
+++ Pekan 03/zhilvia_084/main.java
@@ -1,1 +1,21 @@
-main
+class Person {
+    String nama;
+    int umur;
+
+    void tampilData() {
+        System.out.println("Nama : " + nama);
+        System.out.println("Umur : " + umur);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Person orang1 = new Person();
+
+        orang1.nama = "Zhilvia";
+        orang1.umur = 19;
+
+        orang1.tampilData();
+    }
+}
