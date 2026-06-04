ab9276126bd449f22d139148cf32694a43ca3b3c
Nurulqomariyah1 <qomariamifda@gmail.com>
2026-06-04 15:34:03 +0700

    Add files via upload

--- Pekan 15/Riya_094/Hewan.java
+++ Pekan 15/Riya_094/Hewan.java
@@ -1,1 +1,62 @@
-bebsdb
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+class SuaraHewan {
+
+    protected String nama;
+
+    public SuaraHewan(String nama) {
+        this.nama = nama;
+    }
+
+    public void suara() {
+        System.out.println("Hewan bersuara");
+    }
+}
+
+// Inheritance
+class Kucing extends SuaraHewan {
+
+    public Kucing(String nama) {
+        super(nama);
+    }
+
+    // Polymorphism (Method Overriding)
+    @Override
+    public void suara() {
+        System.out.println(nama + " berkata: Meong...");
+    }
+}
+
+// Inheritance
+class Anjing extends SuaraHewan {
+
+    public Anjing(String nama) {
+        super(nama);
+    }
+
+    // Polymorphism (Method Overriding)
+    @Override
+    public void suara() {
+        System.out.println(nama + " berkata: Guk Guk...");
+    }
+}
+
+public class Hewan {
+
+    public static void main(String[] args) {
+
+        SuaraHewan hewan1 = new Kucing("Kitty");
+        SuaraHewan hewan2 = new Anjing("Doggy");
+
+        hewan1.suara();
+        hewan2.suara();
+    }
+}
