c6541a5e74feb8045145dd5b16561b282a850458
sittiarika17-ui <sittiarika17@gmail.com>
2026-06-04 16:06:41 +0700

    Create Person.java

---
+++ Pekan 03/Arika_087/Person.java
@@ -0,0 +1,21 @@
+class Person {
+    String nama;
+    int umur;
+
+    void tampilData() {
+        System.out.println("Nama : " + nama);
+        System.out.println("Umur : " + umur);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Person orang1 = new Person();
+
+        orang1.nama = "Aricha";
+        orang1.umur = 19;
+
+        orang1.tampilData();
+    }
+}
