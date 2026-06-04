f7ba806cc107238dd161072f4ad4f6fdb91b2188
Yuyanikko <triwahyufajarmalik496@gmail.com>
2026-06-04 15:57:40 +0700

    TugasPekan_03.java

--- Pekan 03/Akun baru_Tri wahyu_095/TugasPekan_03.java
+++ Pekan 03/Akun baru_Tri wahyu_095/TugasPekan_03.java
@@ -1,1 +1,35 @@
 
+import java.util.Scanner;
+
+public class Mahasiswa {
+    
+
+    // 
+    private String name;
+    private int age;
+    private String city;
+
+    // 
+    public Mahasiswa(String name, int age, String city) {
+        this.name = name;
+        this.age = age;
+        this.city = city;
+    }
+
+    // 
+    public void displayInfo() {
+        System.out.println("Nama  : " + name);
+        System.out.println("Umur  : " + age);
+        System.out.println("Kota  : " + city);
+    }
+
+    // 
+    public static void main(String[] args) {
+
+        // 
+        Mahasiswa mhs1 = new Mahasiswa("Tri wahyu fajar malik", 20, "Jombang");
+
+        // 
+        mhs1.displayInfo();
+    }
+}
