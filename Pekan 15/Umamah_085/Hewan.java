b2f4db50d03eac286c7cc0302378ce7b11d32a4a
mahsunatulumamah32-art <mahsunatulumamah32@gmail.com>
2026-06-04 15:19:24 +0700

    Update Hewan.java

--- Pekan 15/Umamah_085/Hewan.java
+++ Pekan 15/Umamah_085/Hewan.java
@@ -1,1 +1,43 @@
-main
+/*@author Taufiq*/
+class Hewan {
+    String nama;
+
+    public Hewan(String nama) {
+        this.nama = nama;
+    }
+    public void suara() {
+        System.out.println("Hewan bersuara...");
+    }
+}
+class Kucing extends Hewan {
+    
+    public Kucing(String nama) {
+        super(nama);
+    }
+
+    @Override//nama method dan isi
+    public void suara() {
+        System.out.println(nama + " berkata: Meong");
+    }
+}
+class Bebek extends Hewan {
+
+    public Bebek(String nama) {
+        super(nama);
+    }
+    
+    @Override
+    public void suara() {
+        System.out.println(nama + " berkata: Guk guk");
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+
+        Hewan h1 = new Kucing("Kitty");
+        Hewan h2 = new Bebek("Wekwek");
+
+        h1.suara();
+        h2.suara();
+    }
+}
