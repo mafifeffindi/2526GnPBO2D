c3803640676f1d4565e793f084875fbfb303cc19
zhilviaqnmk-code <zhilviaqnmk@icloud.com>
2026-06-04 19:27:44 +0700

    Update student.java

--- Pekan 03/zhilvia_084/student.java
+++ Pekan 03/zhilvia_084/student.java
@@ -1,1 +1,11 @@
-zilvi
+package student;
+public class student {
+
+    String name;
+    int age;
+    String city;
+
+    void introduce() {
+    System.out.println("Hi, my name is " + name + " and I am " + age + " years old " + "i from " + city);
+    }
+}
